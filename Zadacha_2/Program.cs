88d8711ea3b007f36b6850a5d83953ffc99b878f
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-02 16:52:10 +0300

    Выполнил задачи

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3 
+
+int a, b, max;
+System.Console.WriteLine("Введите число а");
+a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число b");
+b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    System.Console.WriteLine("max = " + a);
+}
+if (b>a) 
+{
+    System.Console.WriteLine("max = " + b);
+}
+else { 
+    System.Console.WriteLine(a + " = " + b);
+}
---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a, b, c, max;
+
+System.Console.WriteLine("Введите число а");
+a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число b");
+b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число c");
+c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if (max>b){
+    max=max;
+    if (max>c){
+        max=max;
+    }
+}
+if (max<b){
+    max=b;
+    if (max>c){
+        max=max;
+    }
+}
+if (max<c){
+    max=c;
+}
+System.Console.WriteLine(max);
